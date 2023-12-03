566e1784bdb1f66f28ae94aec330102502500253
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2023-12-03 08:28:03 +0300

    User Creation

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,5 +1,18 @@
+import User from "#/models/user";
 import { Router } from "express";
 
 const router = Router()
+
+router.post("/create", async (req, res) => {
+    const { email, password, name } = req.body
+    const user = new User({email, password, name})
+    try { 
+        await user.save()
+        res.json({user})
+    } catch(err) {
+        console.error(err)
+        res.json({error: err})
+    }
+})
 
 export default router
