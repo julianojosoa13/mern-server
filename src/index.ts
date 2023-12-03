e3043354b597fcdb890dadd72d4ce4837e5b3755
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2023-12-03 07:55:33 +0300

    Registering middleware and the first app route: auth

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,15 @@ import 'dotenv/config'
 
 import './db'
 import { MONGO_PORT } from './utils/variables' 
+import authRouter from './routers/auth'
 
 const app = express()
 
+// register our middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/auth', authRouter)
 app.listen(MONGO_PORT, () => {
     console.log("App is listening on PORT " + MONGO_PORT + "...")
 })
