8750de3da10129a51765bddf4b32cd9b21e42655
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2024-04-05 11:16:47 +0300

    Email template and attachment

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,12 +1,15 @@
 import { RequestHandler } from "express"
 import nodemailer from 'nodemailer'
 
+import path from "path"
+
 import { CreateUser } from "#/@types/user"
 import User from "#/models/user"
 import EmailVerificationToken from "#/models/emailVerificationToken"
 
 import { MAILTRAP_PASSWORD, MAILTRAP_USER } from "#/utils/variables"
 import { generateToken } from "#/utils/helper"
+import { generateTemplate } from "#/mail/template"
 
 export const create: RequestHandler = async (req: CreateUser, res) => {
     const { email, password, name } = req.body
@@ -26,15 +29,37 @@ export const create: RequestHandler = async (req: CreateUser, res) => {
 
         const token = generateToken()
 
+        
         const verificationToken = await EmailVerificationToken.create({
             owner: user._id,
             token
         })
+        
+        const welcomeMessage = `Hi ${name}, welcome to Podify! There are so much thing that we do for verified users. Use the given OTP to verify your email. `
 
         transport.sendMail({
             to: user.email,
             from: 'josoajuliano@gmail.com',
-            html: `<h1>Your verification token is ${token}</h1>`
+            html: generateTemplate({
+                title: "Welcome to Podify",
+                banner: "cid:welcome",
+                btnTitle: token,
+                link: "#",
+                logo: "cid:logo",
+                message: welcomeMessage
+            }),
+            attachments: [
+                {
+                    filename: "logo.png",
+                    path: path.join(__dirname,  "../mail/logo.png"),
+                    cid: "logo"
+                },
+                {
+                    filename: "welcome.png",
+                    path: path.join(__dirname,  "../mail/welcome.png"),
+                    cid: "welcome"
+                }
+            ]
         })
 
         res.status(201).json({user})
