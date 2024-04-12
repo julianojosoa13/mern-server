792aa43fd5ca4bfd7e5d2bcd41e9c0ca95d03187
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2024-04-12 19:52:09 +0300

    Email verification

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,9 +1,10 @@
 import { RequestHandler } from "express"
 
-import { CreateUser } from "#/@types/user"
+import { CreateUser, VerifyEmailRequest } from "#/@types/user"
 import User from "#/models/user"
 import { generateToken } from "#/utils/helper"
 import { sendVerificationMail } from "#/utils/mail"
+import EmailVerificationToken from "#/models/emailVerificationToken"
 
 export const create: RequestHandler = async (req: CreateUser, res) => {
     const { email, password, name } = req.body
@@ -19,4 +20,25 @@ export const create: RequestHandler = async (req: CreateUser, res) => {
         console.error(err)
         res.json({error: err})
     }
+}
+
+export const verifyEmail: RequestHandler = async (req: VerifyEmailRequest, res) => {
+    const { token, userId } = req.body
+    const verificationToken = await EmailVerificationToken.findOne({
+        owner: userId   
+    })
+
+    if(!verificationToken) return res.status(403).json({error: "Invalid token"})
+
+    const matched = await verificationToken.compareToken(token)
+
+    if(!matched) return res.status(403).json({error: "Invalid token"})  
+
+    await User.findByIdAndUpdate(userId, {
+        verified: true
+    })
+
+    await EmailVerificationToken.findByIdAndDelete(verificationToken._id)
+
+    res.json({message: "Your email is now verified"})
 }
--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
-import { create } from "#/controller/user";
+import { create, verifyEmail } from "#/controller/user";
 import { validate } from "#/middleware/validator";
 import { CreateUserSchema } from "#/utils/validationSchema";
 
 const router = Router()
 
 router.post("/create", validate(CreateUserSchema), create )
+router.post("/verify-email", verifyEmail )
 
 export default router
